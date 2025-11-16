381e336e2472ecfb47a1e27f7fb3037fd2a80cf8
cc <i@artista.cc>
2025-11-16 14:33:17 +0800

    impl Display for error

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub enum Error {
     Io(String),
     Permission(String),
@@ -7,5 +9,15 @@ pub enum Error {
 impl From<std::io::Error> for Error {
     fn from(value: std::io::Error) -> Self {
         Error::Io(value.to_string())
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::Io(msg) => write!(f, "I/O error: {}", msg),
+            Error::Permission(msg) => write!(f, "Permission error: {}", msg),
+            Error::Command(msg) => write!(f, "Command error: {}", msg),
+        }
     }
 }
